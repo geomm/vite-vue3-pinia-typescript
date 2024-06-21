b347ea87a42a540b1c3ffa7dd9cac2c2576b82f3
geomm <ge.melampianakis@gmail.com>
2024-06-21 03:40:47 +0300

    pagination input disabled state fix

--- rick-and-morty-UI/src/components/UICompoents/InputComponent.vue
+++ rick-and-morty-UI/src/components/UICompoents/InputComponent.vue
@@ -20,14 +20,7 @@ button {
 </style>
 <template>
   <div class="mat-input">
-    <input
-      :type="type"
-      :id="id"
-      v-model="value"
-      v-on:focusout="updateValue"
-      required
-      @input="onUserInput"
-    />
+    <input :type="type" :id="id" v-model="value" v-on:focusout="updateValue" required />
     <span class="highlight"></span>
     <span class="bar"></span>
     <label :for="label">{{ label }}</label>
@@ -40,7 +33,7 @@ button {
 </template>
 <script lang="ts">
 import type { InputTypes, InputValue } from '@/models/input-types.model';
-import { defineComponent, ref, type PropType } from 'vue';
+import { defineComponent, ref, watch, type PropType } from 'vue';
 
 export default defineComponent({
   name: 'InputComponent',
@@ -64,16 +57,16 @@ export default defineComponent({
       } else if (event instanceof Event) {
         emit('update:inputValue', value);
       }
+      console.log('valval:', value.value);
     };
 
-    const onUserInput = () => {
-      isButtonDisabled.value = value.value?.toString().trim() === '' || value.value === null;
-    };
+    watch(value, (newVal) => {
+      isButtonDisabled.value = newVal?.toString().trim() === '' || !newVal;
+    });
 
     return {
       updateValue,
       value,
-      onUserInput,
       isButtonDisabled
     };
   }
