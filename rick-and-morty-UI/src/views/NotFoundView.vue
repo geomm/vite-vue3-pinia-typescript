e708786c12ca7bc017674ce790e676c6d146728b
geomm <ge.melampianakis@gmail.com>
2024-06-23 00:25:05 +0300

    404

---
+++ rick-and-morty-UI/src/views/NotFoundView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex">
+    <h1>Page not found! <RouterLink to="/">Go to the Characters list!</RouterLink></h1>
+  </main>
+</template>
